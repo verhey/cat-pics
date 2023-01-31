52436d4e27c90484b7d7b653906bf46b04a5651d
Dean Verhey <deanverhey@gmail.com>
2023-01-30 21:45:45 -0800

    Add prettier - zamboni lint

--- functions/cat.js
+++ functions/cat.js
@@ -1,33 +1,33 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const pickCat = () => {
-    const basePath = "functions/cats"
-    const allImages = fs.readdirSync(basePath)
-    const cat = allImages[Math.floor(Math.random() * allImages.length)]
+  const basePath = "functions/cats";
+  const allImages = fs.readdirSync(basePath);
+  const cat = allImages[Math.floor(Math.random() * allImages.length)];
 
-    return [basePath, cat].join("/")
-}
+  return [basePath, cat].join("/");
+};
 
 export const handler = async () => {
-    let image
+  let image;
 
-    try {
-        cat = pickCat()
-        image = fs.readFileSync(cat)
-    } catch (error) {
-        console.log('error', error)
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: error.message })
-        }
-    }
-
+  try {
+    cat = pickCat();
+    image = fs.readFileSync(cat);
+  } catch (error) {
+    console.log("error", error);
     return {
-        statusCode: 200,
-        headers: {
-            "Content-Type": "image/jpg",
-        },
-        body: image.toString("base64"),
-        isBase64Encoded: true,
-    }
-}
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "image/jpg",
+    },
+    body: image.toString("base64"),
+    isBase64Encoded: true,
+  };
+};
